c0a1554f2ad965b03b3de2ff15ef2f1b6fd36c63
Jonathan Meyer <jmeyer@swri.org>
2017-11-24 16:48:26 -0600

    Added a few more example robot configurations.

--- include/opw_kinematics/opw_parameters_examples.h
+++ include/opw_kinematics/opw_parameters_examples.h
@@ -23,7 +23,62 @@ Parameters<T> makeIrb2400_10()
   return p;
 }
 
+template <typename T>
+__attribute__((deprecated("UN-TESTED")))
+Parameters<T> makeFanucR2000iB_200R()
+{
+  Parameters<T> p;
+  p.a1 = T(0.720);
+  p.a2 = T(-0.225);
+  p.b =  T(0.000);
+  p.c1 = T(0.600);
+  p.c2 = T(1.075);
+  p.c3 = T(1.280);
+  p.c4 = T(0.235);
 
+  // WARNING: This is a guess! I don't know the offets.
+  p.offsets[2] = -M_PI / 2.0;
+
+  return p;
+}
+
+template <typename T>
+__attribute__((deprecated("UN-TESTED")))
+Parameters<T> makeKukaKR6_R700_sixx()
+{
+  Parameters<T> p;
+  p.a1 = T(0.025);
+  p.a2 = T(-0.035);
+  p.b =  T(0.000);
+  p.c1 = T(0.400);
+  p.c2 = T(0.315);
+  p.c3 = T(0.365);
+  p.c4 = T(0.080);
+
+  // WARNING: This is a guess! I don't know the offets.
+  p.offsets[2] = -M_PI / 2.0;
+
+  return p;
+}
+
+template <typename T>
+__attribute__((deprecated("UN-TESTED")))
+Parameters<T> makeStaubliTX40()
+{
+  Parameters<T> p;
+  p.a1 = T(0.000);
+  p.a2 = T(0.000);
+  p.b =  T(0.035);
+  p.c1 = T(0.320);
+  p.c2 = T(0.225);
+  p.c3 = T(0.225);
+  p.c4 = T(0.065);
+
+  // WARNING: This is a guess! I don't know the offets.
+  p.offsets[2] = -M_PI / 2.0;
+
+  return p;
+}
 
 } // ns opw_kinematics
 

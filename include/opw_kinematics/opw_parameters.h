b46d955374df8e55690999f0ef5c0a795e5863bc
mripperger <michael.ripperger@swri.org>
2019-05-23 13:52:56 -0500

    Uncommented check for floating point number

--- include/opw_kinematics/opw_parameters.h
+++ include/opw_kinematics/opw_parameters.h
@@ -9,8 +9,8 @@ namespace opw_kinematics
 template <typename T>
 struct Parameters
 {
-//  static_assert(std::is_floating_point<T>::value,
-//                "OPW parameters must be templatized with floating point type");
+  static_assert(std::is_floating_point<T>::value,
+                "OPW parameters must be templatized with floating point type");
 
   T a1, a2, b, c1, c2, c3, c4;
   T offsets[6];
